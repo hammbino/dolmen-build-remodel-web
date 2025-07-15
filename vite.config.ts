fac8595d7986efe09237f49a30446ed2e0c3ad84
Jeffrey Hammond <hammondjeffrey@gmail.com>
2025-07-15 10:02:48 -0600

    updated repo name

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/dolmen-build-remodel-web/',
+  base: '/dolmen-website-redo/',
   server: {
     host: "::",
     port: 8080,
